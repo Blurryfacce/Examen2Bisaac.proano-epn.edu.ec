c03d9bb898515eba2e94a963817261cb9e4c5eb7
Isaac <Blurryface0611@gmail.com>
2023-03-06 12:05:15 -0500

    Exaen

---
+++ src/isaac_proano.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "..\lib\ipcolor.h"
+
+using namespace std;
+
+int main()
+{
+    
+
+
+
+    return 0;
+}
---
+++ src/readme.md
@@ -0,0 +1,7 @@
+### EXAMEN 2BIMESTRE
+## DATOS
+isaac Proaño
+
+Primer semestre
+
+https://github.com/Blurryfacce/Isaac_proano_2B.git
